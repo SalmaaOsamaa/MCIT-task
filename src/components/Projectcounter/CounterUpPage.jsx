1b4b9cf530b51d7b4d9fa23a6b13caca2218d817
SalmaaOsamaa <79584660+SalmaaOsamaa@users.noreply.github.com>
2022-08-30 14:33:55 +0200

    counter new chnages

--- src/components/Projectcounter/CounterUpPage.jsx
+++ src/components/Projectcounter/CounterUpPage.jsx
@@ -1,9 +1,37 @@
 import React, { useState } from "react";
 import CountUp from "react-countup";
 import "./Counteruppage.css";
-import services from '../../images/services-3.png'
+
 import ScrollTrigger from "react-scroll-trigger";
 function CounterUpPage() {
+  const counters = [
+    {
+    id:1,
+    image: "newimages/services-3.png",
+    count: 225,
+    title:"completed Project"
+  },
+  {
+    id:2,
+    image: "newimages/about-2.png",
+    count: 109,
+    title:"National Award"
+  },
+  {
+    id:3,
+    image: "newimages/about-3.png",
+    count: 220,
+    title:"Team Members"
+  },
+  {
+    id:4,
+    image: "newimages/about-1.png",
+    count: 220,
+    title:"Happy Client "
+  },
+
+
+]
   const [counterOn, setCounterOn] = useState(false);
   return (
     <ScrollTrigger
@@ -12,76 +40,30 @@ function CounterUpPage() {
     >
       <div
         className="counterup"  
-      >
+      > 
+      {counters.map((counter)=>(
         <div className="container row">
-          <div className="col-4">
-            <img className="image" src= {services} alt="" />
-          </div>
-          <div className="col-8">
-            <div className="row">
-              <h1>  {counterOn && (
-              <CountUp start={0} end={225} duration={2} delay={0} />
-            )}
-            K</h1>
-            </div>
-            <div className="row">
-            <p>Completed Project</p>
-            </div>
-          </div>
-          
+        <div className="col-4">
+          <img className="image" src= {counter.image} alt="" />
         </div>
-        <div className="container row">
-          <div className="col-4">
-            <img className="image" src= {services} alt="" />
+        <div className="col-8">
+          <div className="row">
+            <h1>  {counterOn && (
+            <CountUp start={0} end={counter.count} duration={2} delay={0} />
+          )}
+          K</h1>
           </div>
-          <div className="col-8">
-            <div className="row">
-              <h1>  {counterOn && (
-              <CountUp start={0} end={250} duration={2} delay={0} />
-            )}
-            +</h1>
-            </div>
-            <div className="row">
-            <p>Completed Project</p>
-            </div>
+          <div className="row">
+          <p>{counter.title}</p>
           </div>
-          
-        </div>
-        <div className="container row">
-          <div className="col-4">
-            <img className="image" src= {services} alt="" />
-          </div>
-          <div className="col-8">
-            <div className="row">
-              <h1>  {counterOn && (
-              <CountUp start={0} end={109} duration={2} delay={0} />
-            )}
-            +</h1>
-            </div>
-            <div className="row">
-            <p>Completed Project</p>
-            </div>
-          </div>
-          
-        </div>
-        <div className="container row">
-          <div className="col-4">
-            <img className="image" src= {services} alt="" />
-          </div>
-          <div className="col-8">
-            <div className="row">
-              <h1>  {counterOn && (
-              <CountUp start={0} end={220} duration={2} delay={0} />
-            )}
-            K</h1>
-            </div>
-            <div className="row">
-            <p>Completed Project</p>
-            </div>
-          </div>
-          
         </div>
         
+      </div>
+      ))}
+        
+     
+       
+       
         
       </div>
     </ScrollTrigger>
